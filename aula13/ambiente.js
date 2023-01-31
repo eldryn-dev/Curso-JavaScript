0a0756aae7f00b7e43338e318e4771c56ebb0bd0
Eldryn Santos <eldryn.dev@gmail.com>
2023-01-31 17:34:22 -0300

    while e for

---
+++ aula13/ambiente.js
@@ -0,0 +1,20 @@
+/*
+var c = 0
+while (c < 6) {
+    console.log('Tudo bem?')
+    c++   
+}
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+var c = 0
+do {
+    console.log('Tudo bem?')
+    c++   
+} while (c<6)
